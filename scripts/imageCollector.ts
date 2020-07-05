761ffc95ae8f336d114d3d32c27bfdd2b3c28100
Isaac.Pan <Isaac.Pan@skylinetw.com.tw>
2020-07-05 13:52:49 +0800

    refactor code

--- scripts/imageCollector.ts
+++ scripts/imageCollector.ts
@@ -1,48 +1,56 @@
-import { resolve, join } from 'https://deno.land/std/path/mod.ts'
-import { red, green } from 'https://deno.land/std/fmt/colors.ts'
-import { range } from 'https://deno.land/x/fae/mod.ts'
+import { resolve, join } from "https://deno.land/std/path/mod.ts";
+import { red, green } from "https://deno.land/std/fmt/colors.ts";
+import { range } from "https://deno.land/x/fae/mod.ts";
 
-const imageRootPath =
-  'https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/04-explode-tips'
-const distDir = resolve(Deno.cwd(), '_testImages')
-const imgSuffix = '.jpg'
-const limit = 300
+const config = {
+  imageRootPath:
+    "https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/04-explode-tips",
+  distDir: resolve(Deno.cwd(), "_testImages"),
+  imgSuffix: ".jpg",
+  limit: 300,
+};
 
-const downloadImage = async (image: string, distPath: string) => {
-  const res = await fetch(image)
-  const body = new Uint8Array(await res.arrayBuffer())
-  await Deno.writeFile(distPath, body)
-  console.log(green('[success]'), distPath)
+const message = {
+  success: green("\nDownload Complete."),
+  error: red("\nDownload Failed."),
+};
+
+async function downloadImage(image: string, distPath: string): Promise<void> {
+  const res = await fetch(image);
+  const body = new Uint8Array(await res.arrayBuffer());
+  await Deno.writeFile(distPath, body);
+  console.log(green("[success]"), distPath);
 }
 
-const collectImages = async (
-  imageRootPath: string,
-  distDir: string,
-  imgSuffix: string,
-  limit: number
-) => {
-  await Deno.mkdir(distDir, { recursive: true })
-  return Promise.all(
+async function collectImages({
+  imageRootPath,
+  distDir,
+  imgSuffix,
+  limit,
+}: typeof config): Promise<PromiseSettledResult<void>[]> {
+  return Promise.allSettled(
     range(0, limit).map((index) => {
       try {
-        const filename = ('' + index).padStart(4, '0') + imgSuffix
-        const image = join(imageRootPath, filename)
-        const distPath = resolve(distDir, filename)
-        return downloadImage(image, distPath)
+        const filename = ("" + index).padStart(4, "0") + imgSuffix;
+        const image = join(imageRootPath, filename);
+        const distPath = resolve(distDir, filename);
+        return downloadImage(image, distPath);
       } catch (err) {
-        console.log(red('[error]'), err)
-        throw err
+        console.log(red("[error]"), err);
+        throw err;
       }
-    })
-  )
+    }),
+  );
 }
 
-const successMessage = green('\nDownload Complete.')
-const errorMessage = red('\nDownload Failed.')
-
 try {
-  await collectImages(imageRootPath, distDir, imgSuffix, limit)
-  console.log(successMessage)
-} catch {
-  console.log(errorMessage)
+  await Deno.mkdir(config.distDir, { recursive: true });
+  const results = await collectImages(config);
+  console.log(
+    results.some((r) => r.status === "rejected")
+      ? message.error
+      : message.success,
+  );
+} catch (err) {
+  console.error(err);
 }
